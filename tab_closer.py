4bde520907be435986975baaa930f2051e35445b
Simon Petersson <simon@Simons-MacBook-Pro-3.local>
2016-10-07 15:36:32 -0400

    Add Git Manager to check changes in git

--- tab_closer.py
+++ tab_closer.py
@@ -4,17 +4,24 @@ from itertools import chain
 # Append the folder that contains all plugins
 sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
 
-# Construct the diff that we want to observe.
-diff = repo.commit("HEAD@{1}").diff("HEAD")
+from git import Repo
 
-# Get the deletions from the difference
-deletions = diff.iter_change_type('D')
+class GitManager:
+    def __init__(self, repo_directory):
+        # Construct a repo object from the path that we were given
+        self.repo = Repo(repo_directory)
 
-# Get the renames that were performed
-renames = diff.iter_change_type('R')
+    def get_difference(self):
+        # Construct the diff that we want to observe
+        diff = self.repo.commit("HEAD@{1}").diff("HEAD")
 
-# Now chain the deletions and renames together, so that we can iterate over them
-file_changes = chain(deletions, renames)
+        # Get the files that were deleted
+        deleted = diff.iter_change_type('D')
 
-for change in file_changes:
-    print(change)
+        # Get the files that were renamed
+        renamed = diff.iter_change_type('R')
+
+        # Now chain the deleted and renamed files together into a new generator
+        # and return
+        return chain(deleted, renamed)
+
