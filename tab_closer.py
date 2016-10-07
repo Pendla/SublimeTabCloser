f2b9d5a7a1f79c1373b83b4fb94a881d16f0cdea
Simon Petersson <simon@Simons-MacBook-Pro-3.local>
2016-10-07 15:37:04 -0400

    Add proof of concept implementation for closing tabs

--- tab_closer.py
+++ tab_closer.py
@@ -25,3 +25,23 @@ class GitManager:
         # and return
         return chain(deleted, renamed)
 
+class TabCloserEventListener(sublime_plugin.EventListener):
+    git_manager = None
+
+    def on_activated_async(self, view):
+        if self.git_manager is None:
+            project_dir = self.get_project_dir(view)
+            self.git_manager = GitManager(project_dir)
+
+        # Get the git difference
+        differences = self.git_manager.get_difference()
+        for diff in differences:
+            for tab in view.window().views():
+                print(tab.file_name())
+                print(diff.a_path)
+                if tab.file_name() is not None and tab.file_name().endswith(diff.a_path):
+                    tab.set_scratch(True)
+                    tab.close()
+
+    def get_project_dir(self, view):
+        return view.window().extract_variables()['folder']
